1afdfe5cbe22e30aa2b0b6fb03f6ee26c688a2e2
Nicholas Furness <nfurness@nixta.com>
2012-10-11 15:54:41 -0400

    Broke things down properly into two targets. A static library (esriQuickStartLib.a) and an application (esriQuickStartApp.app) which is dependent on the library.

--- QuickStartApp/UI/EQSBorderedButton.m
+++ QuickStartApp/UI/EQSBorderedButton.m
@@ -59,14 +59,4 @@
         self.backgroundColor = self.stashedBackgroundColor;
     }
 }
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
-
 @end
