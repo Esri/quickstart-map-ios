7a2fd953ae9e38a7bd0fd115cf06cd0b801257a4
Nicholas Furness <nfurness@nixta.com>
2012-11-29 19:30:52 -0500

    Add the QuickStart Library and set up the map.

--- Samples/Basemaps/Basemaps/EQSViewController.m
+++ Samples/Basemaps/Basemaps/EQSViewController.m
@@ -6,10 +6,12 @@
 //  Copyright (c) 2012 ESRI. All rights reserved.
 //
 
+#import <ArcGIS/ArcGIS.h>
+#import <EsriQuickStart/EsriQuickStart.h>
 #import "EQSViewController.h"
 
 @interface EQSViewController ()
-
+@property (strong, nonatomic) IBOutlet AGSMapView *mapView;
 @end
 
 @implementation EQSViewController
@@ -18,6 +20,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self.mapView setBasemap:EQSBasemapTypeTopographic];
+    [self.mapView zoomToLevel:15 withLat:40.7262 lon:-73.98186 animated:YES];
 }
 
 - (void)didReceiveMemoryWarning
