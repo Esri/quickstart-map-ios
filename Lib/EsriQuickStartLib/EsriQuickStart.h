0003eb1bd335617f75e437158219a33f0794b1df
Nicholas Furness <nfurness@nixta.com>
2012-12-06 11:01:27 -0500

    Cosmetic change to EsriQuickStart.h

--- Lib/EsriQuickStartLib/EsriQuickStart.h
+++ Lib/EsriQuickStartLib/EsriQuickStart.h
@@ -4,7 +4,7 @@
 //
 //  Created by Nicholas Furness on 10/13/12.
 //
-//
+//  Built into the framework, to be accessed with #import <EsriQuickStart/EsriQuickStart.h>
 
 #ifndef EsriQuickStart_EsriQuickStart_h
 #define EsriQuickStart_EsriQuickStart_h
@@ -14,11 +14,12 @@
 #import "AGSMapView+EQSRouteDisplay.h"
 #import "AGSMapView+EQSGraphics.h"
 #import "AGSMapView+EQSNavigation.h"
-#import "EQSDefaultSymbols.h"
-#import "EQSGeoServices.h"
 
 #import "AGSMapView+EQSGeneralUtilities.h"
 #import "AGSPoint+EQSGeneralUtilities.h"
+
+#import "EQSDefaultSymbols.h"
+#import "EQSGeoServices.h"
 
 #import "EQSHelper.h"
 
